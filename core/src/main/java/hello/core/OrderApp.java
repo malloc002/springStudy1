1b41f068fcb81c44b4c29c321f0089597a62a873
DESKTOP-CGH48O5\laure <laurenkim002@naver.com>
2023-03-24 18:26:55 +0900

    [김혜민]

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -1,14 +1,10 @@
 package hello.core;
 
-import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
@@ -22,7 +18,7 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
         System.out.println("order = " + order);
         System.out.println("orderPrice = " + order.calculatePrice());
     }
