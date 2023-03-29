8c074ce35bed02a32f1e2e1486dff210a0378783
DESKTOP-CGH48O5\laure <laurenkim002@naver.com>
2023-03-29 20:01:12 +0900

    [김혜민] - 4. 컨테이너에 등록된 모든 빈 조회 - 4. 스프링 빈 조회 - 기본 - 4. 스프링 빈 조회 - 동일한 타입이 둘 이상

---
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,48 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    //같은 타입 스트링 빈이 두개면 오류 발생함
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2(){
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX(){
+//        MemberService memberService = ac.getBean("xxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class, 
+                () -> ac.getBean("xxx", MemberService.class));
+        // () 오른쪽이 실행 했을때 괄호 안에 있는 에러가 발생해야지 성공한 것
+    }
+}
